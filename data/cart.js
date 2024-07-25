d674d33de6bfec6ef10162813f3218380098944c
ericknjogu <njoguerick3@gmail.com>
2024-07-25 16:43:05 +0300

    make the update button interactive

--- data/cart.js
+++ data/cart.js
@@ -49,3 +49,31 @@ export function removeFromCart(productId) {
 
   saveToStorage();
 }
+
+
+ export function calculateCartQuantity() {
+  let cartQuantity=0;
+
+  cart.forEach((cartItem)=>{
+    cartQuantity+=cartItem.quantity;
+  });
+
+  return cartQuantity
+}
+
+export function updateQuantity(productId, newQuantity){
+  let matchingItem;
+
+  cart.forEach((cartItem)=>{
+
+    if (productId===cartItem.productId) {
+      matchingItem=cartItem;
+    }
+  });
+
+  
+  matchingItem.quantity=newQuantity;
+
+
+  saveToStorage();
+}
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart, addToCart } from "../data/cart.js";
+import { cart, addToCart, calculateCartQuantity } from "../data/cart.js";
 import {products} from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -64,12 +64,9 @@ document.querySelector('.js-products-grid').innerHTML=ProductsHTML;
 let addedMessageTimeoutId;
 
 function updateCartQuantity() {
-  let cartQuantity=0;
-
-  cart.forEach((cartItem)=>{
-    cartQuantity+=cartItem.quantity;
-  });
-
+  
+  const cartQuantity=calculateCartQuantity();
+  
   document.querySelector('.js-cart-quantity')
     .innerHTML=cartQuantity;   
       
