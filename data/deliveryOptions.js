cd69127673356d649a5729073b602a7c96035b8c
ericknjogu <njoguerick3@gmail.com>
2024-07-26 16:12:22 +0300

    save data and generate HTML for payment summary

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -10,4 +10,16 @@ export const deliveryOptions=[{
   id: '3',
   deliveryDays: 1,
   priceCents: 999
-}]
+}];
+
+export function getDeliveryOption(deliveryOptionId) {
+  let deliveryOption;
+
+  deliveryOptions.forEach((option)=>{
+    if (option.id===deliveryOptionId) {
+      deliveryOption=option;
+    }
+  });
+
+  return deliveryOption || deliveryOptions[0];
+}
