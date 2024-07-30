4cc2e95f097f8e8b92f4fdaa75086dd7cb1e33cc
ericknjogu <njoguerick3@gmail.com>
2024-07-30 17:21:06 +0300

    rename test folders

--- tests-jasmine/tests-simple/moneyTest.js
+++ tests-jasmine/tests-simple/moneyTest.js
@@ -1,4 +1,4 @@
-import { formatCurrency } from "../scripts/utils/money.js";
+import { formatCurrency } from "../../scripts/utils/money.js";
 
 console.log('Test Suite: FormatCurrency');
 console.log('converts into decimal');
