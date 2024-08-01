d2771f617d02ad77f5204700de29b97387c9f9f1
ericknjogu <njoguerick3@gmail.com>
2024-08-01 13:15:15 +0300

    make methods and variable private

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,16 +1,16 @@
 class Cart {
   cartItems;
-  localStorageKey;
+  #localStorageKey;
 
   constructor(localStorageKey){
-    this.localStorageKey=localStorageKey;
+    this.#localStorageKey=localStorageKey;
     
-    this.loadFromStorage();
+    this.#loadFromStorage();
    
   }
 
-  loadFromStorage(){
-    this.cartItems= JSON.parse(localStorage.getItem(this.localStorageKey));
+  #loadFromStorage(){
+    this.cartItems= JSON.parse(localStorage.getItem(this.#localStorageKey));
   
     if (!this.cartItems) {
       this.cartItems=[{
@@ -27,7 +27,7 @@ class Cart {
   }
 
   saveToStorage() {
-    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+    localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
   }
 
   addToCart(productId,quantity) {
@@ -118,7 +118,6 @@ class Cart {
 
 const cart=new Cart('cart-oop');
 const businessCart=new Cart('cart-business');
-
 
 
 console.log(cart);
