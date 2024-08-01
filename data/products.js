e23ab9fe0c8538365bb06e34f4ea82cc250e2816
ericknjogu <njoguerick3@gmail.com>
2024-08-01 19:59:57 +0300

    use inheritance and polymorphism to insert chart size link

--- data/products.js
+++ data/products.js
@@ -33,26 +33,30 @@ class Product{
   getPrice(){
     return `$${formatCurrency(this.priceCents)}`;
   }
+
+  extraInfoHTML(){
+    return ''; 
+  }
 }
 
-/*const product1=new Product({
-  id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090,
-  keywords: [
-    "socks",
-    "sports",
-    "apparel"
-  ]
-});
+class Clothing extends Product{
+  sizeChartLink;
 
-console.log(product1);
-*/
+  constructor(productDetails){
+    super(productDetails);
+    this.sizeChartLink=productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML(){
+    return `
+      <a href="${this.sizeChartLink}" target="_blank">
+      size chart
+      </a>
+    `;
+  }
+}
+
+
 
 export const products = [
   {
@@ -714,6 +718,10 @@ export const products = [
     ]
   }
 ].map((productDetails)=>{
+
+  if (productDetails.type==='clothing'){
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
 
