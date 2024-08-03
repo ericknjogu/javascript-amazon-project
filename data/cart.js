f71698d626a4590aebac4c4c9fa78e23abdc84ca
ericknjogu <njoguerick3@gmail.com>
2024-08-03 09:21:08 +0300

    using promises instead of callbacks

--- data/cart.js
+++ data/cart.js
@@ -104,3 +104,17 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
   saveToStorage();
   
 }
+
+export function loadCart(fun) {
+  const xhr=new XMLHttpRequest();
+
+  xhr.addEventListener('load',()=>{
+    console.log(xhr.response);
+    
+    fun();
+  })
+
+  xhr.open('GET', 'https://supersimplebackend.dev/cart');
+  xhr.send();
+
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,14 +2,40 @@ import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js'
 //import '../data/backend-practice.js'
 
-loadProducts(()=>{
+new Promise((resolve)=>{ 
+  loadProducts(()=>{ 
+    resolve();
+  });
+}).then(()=>{
+  return new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve();
+    });
+  })
+}).then(()=>{
   renderOrderSummary();
 
   renderPaymentSummary();
 
   renderCheckoutHeader();
 });
+
+
+/*
+loadProducts(()=>{
+  loadCart(()=>{
+    renderOrderSummary();
+
+    renderPaymentSummary();
+
+    renderCheckoutHeader();
+  })
+  
+});
+*/
+
 
