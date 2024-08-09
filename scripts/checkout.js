7aa2fff196a2a878ddc23f2a09f2702c9983676e
ericknjogu <njoguerick3@gmail.com>
2024-08-09 20:08:31 +0300

    convert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,18 +1,34 @@
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadProducts } from "../data/products.js";
+import {  loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 import { updateCartQuantity } from "./checkout/updateCartQuantity.js";
 //import '../data/cart-class.js'
 //import '../data/backend-practice.js'
 
-Promise.all([
-  new Promise((resolve)=>{ 
-    loadProducts(()=>{ 
-      resolve('value 1');
+async function loadPage(){
+  
+
+  await loadProductsFetch();
+
+  await new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve();
     });
-  }),
+  });
+
+  updateCartQuantity(); 
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+  
+}
+loadPage();
+
+/*
+Promise.all([
+ loadProductsFetch(),
   new Promise((resolve)=>{
     loadCart(()=>{
       resolve();
@@ -24,7 +40,8 @@ Promise.all([
   renderCheckoutHeader();
   renderOrderSummary();
   renderPaymentSummary();
-})
+});
+*/
 
 /*
 new Promise((resolve)=>{ 
